e8f5532563e29ffe37a0eacb71f3145cfc27eb69
iNekox3 <inekox3@hotmail.com>
2018-02-12 19:52:52 +0800

    SortCommand: add message constants values
    Includes standard command word, its usage help message and message upon successful execution.

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -4,4 +4,11 @@ package seedu.addressbook.commands;
  * Sort the people in the address book.
  */
 public class SortCommand extends Command {
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sort all persons in the address book "
+            + "by alphabetical order.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SUCCESS = "Address book has been sorted!";
 }
