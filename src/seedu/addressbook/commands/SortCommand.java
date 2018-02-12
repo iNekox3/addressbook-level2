097e1a9d41b1da0a847d811f957934941d3d23e0
iNekox3 <inekox3@hotmail.com>
2018-02-12 20:04:10 +0800

    SortCommand: add constructor, execute method and minor change to class description
    Execute method to call sort method in AddressBook and return a success message command result.

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,14 +1,22 @@
 package seedu.addressbook.commands;
 
 /**
- * Sort the people in the address book.
+ * Sort the people in address book.
  */
 public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sort all persons in the address book "
-            + "by alphabetical order.\n"
+            + "by names in alphabetical order.\n"
             + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SUCCESS = "Address book has been sorted!";
+
+    public SortCommand() {}
+
+    @Override
+    public CommandResult execute() {
+        addressBook.sort();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
 }
