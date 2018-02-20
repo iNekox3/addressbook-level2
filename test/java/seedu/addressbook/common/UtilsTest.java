596dc6ef16de7138663af98ec157b64c52497b49
iNekox3 <inekox3@hotmail.com>
2018-02-21 01:03:35 +0800

    UtilsTest: add test for isAnyNull
    Tests for containing no null objects and containing some null objects.

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -43,4 +43,28 @@ public class UtilsTest {
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
+    
+    @Test
+    public void elementsIsAnyNull() throws Exception {
+        // no null objects
+        assertNoNull("");
+        assertNoNull("", "abc", "a");
+        assertAreUnique("abc", "ab", "a");
+        assertNoNull(1);
+        assertAreUnique(1, 2);
+        
+        // some null objects
+        assertHasNull((Object) null);
+        assertHasNull(null, 1, new Integer(1));
+        assertHasNull(null, null);
+        assertHasNull(null, "a", "b", null);
+    }
+
+    private void assertNoNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    }
+    
+    private void assertHasNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
+    }1
 }
